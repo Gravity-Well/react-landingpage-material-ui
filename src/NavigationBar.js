c7d73325dbafce96a463f100d15eadb40fb28246
robert <bobtomlin@gmail.com>
2021-02-18 08:12:10 -0500

    nav2

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,8 +1,22 @@
 import { Box, Container, Toolbar, Typography , Link} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
 import React from 'react'
 
-
+const useStyles = makeStyles((theme)=>({
+    menuOption: {
+        padding: theme.spacing(1)
+    },
+    menuBox: {
+        display: 'flex',
+        flexDirection: 'column',    // small screens
+        [theme.breakpoints.up('md')]: {
+            flexDirection: 'row' //full screen, md=midpoint
+        }
+    }
+}))
 export const NavigationBar = () => {
+    const classes = useStyles()
+
     return (
         <div>
            <Container>
@@ -10,9 +24,10 @@ export const NavigationBar = () => {
                 <Typography>
                     Gravity Well Products
                 </Typography>
-                <Box>
+                <Box className={classes.menuBox}>
                     {['home','courses','sign-up'].map((menuoption)=>(
-                        <Link component='button' variant='body1'>
+                        <Link component='button' variant='body1'
+                        className={classes.menuOption}>
                             {menuoption.toUpperCase()}
                         </Link>
                     ))}
